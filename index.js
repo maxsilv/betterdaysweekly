2d72716274835aa7504da6e3e731b8b8940159a6
Max Silvers <maxsilv@umich.edu>
2018-03-28 18:33:00 -0400

    added about page

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
 	res.render('pages/index');
 });
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+})
+
 
 app.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
